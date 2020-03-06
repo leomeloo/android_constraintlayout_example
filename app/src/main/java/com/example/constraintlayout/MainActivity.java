87f601923ad198eb9fa3270083ac5f4a40a5f231
Leonardo Melo <leonardo.jesus@hdi.com.br>
2020-03-06 16:50:28 -0300

    example com site do Corinthians

---
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt1 = findViewById(R.id.button);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.corinthians.com.br"));
+                startActivity(intent);
+            }
+        });
+    }
+}
